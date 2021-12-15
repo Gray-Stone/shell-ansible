63dc65098bb7ffdfa2a9ee3630865d1699d98008
leo chen <lchen@rtr.ai>
2021-12-15 15:02:28 -0500

    updated tilix setting

--- Desktop-play/user-configs/dconf-configs/load_tilix_dconf.sh
+++ Desktop-play/user-configs/dconf-configs/load_tilix_dconf.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Generate with (from aisnble home dir) dconf dump /com/gexperts/Tilix/ > Desktop-play/user-configs/dconf-configs/tilix_dconf 
+
 echo "loading tilix setting into dconf under /com/gexperts/Tilix/"
 dconf load /com/gexperts/Tilix/ < tilix_dconf
